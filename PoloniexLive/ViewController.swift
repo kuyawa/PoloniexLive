5cdd90e4221f5fd45a47c5c5ac092a64b1adae5a
kuyawa <haxapp@gmail.com>
2017-11-08 10:24:32 -0400

    Initial Commit

---
+++ PoloniexLive/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PoloniexLive
+//
+//  Created by Laptop on 11/8/17.
+//  Copyright © 2017 Armonia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
